86f1700f64832348c737f949c0e77a6e18eedf18
alex <alex.ioan.dragomir@gmail.com>
2022-12-05 00:31:21 +0200

    day3

---
+++ day3/day3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Part 1
+Console.WriteLine(File.ReadAllLines("input.txt")
+  .Select(l => (l.Take(l.Length/2), l.TakeLast(l.Length/2)))
+  .Sum(l => l.Item1.Intersect(l.Item2).Sum(i => i < 'a' ? i - 'A' + 27 : i - 'a' + 1)));
+
+// Part 2
+
+var groups = File.ReadAllLines("input.txt")
+  .Select((l, i) => (l, i))
+  .GroupBy(l => l.i / 3)
+  .Select(g => g.Select(l => l.l)
+  .ToArray());
+
+int total = 0;
+foreach(var g in groups)
+{
+  var found = g[0].Intersect(g[1]).Intersect(g[2]).First();
+  total += found < 'a' ? found - 'A' + 27 : found - 'a' + 1;
+}
+
+Console.WriteLine(total);
