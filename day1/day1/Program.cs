4363113667fcda321d0fd2b5ddfde2dccb5e7065
alex <alex.ioan.dragomir@gmail.com>
2022-12-04 12:33:15 +0200

    day1

---
+++ day1/day1/Program.cs
@@ -0,0 +1,12 @@
+﻿
+// Part 1
+Console.WriteLine(File.ReadAllText("input.txt")
+  .Split("\r\n\r\n")
+  .Select(g => g.Split("\r\n").Select(l => int.Parse(l)).Sum())
+  .Max());
+
+// Part 2
+Console.WriteLine(File.ReadAllText("input.txt")
+  .Split("\r\n\r\n")
+  .Select(g => g.Split("\r\n").Select(l => int.Parse(l)).Sum())
+  .OrderBy(s => s).TakeLast(3).Sum());
